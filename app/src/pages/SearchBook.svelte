911aa2087801a1605aa123f2bdfbcdfe0e3af2b3
夏より冬派 <>
2023-12-20 23:07:08 +0900

    feat: コンポーネント切り出し、もっと見る追加

--- app/src/pages/SearchBook.svelte
+++ app/src/pages/SearchBook.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
   import SearchBar from '../components/SearchBar.svelte'
+  import Spinner from '../components/Spinner.svelte';
+  import BookCard from '../components/BookCard.svelte';
 
-  import type { BookItem, Result } from '../repositories/book';
+  import InfiniteScroll from "svelte-infinite-scroll"
+
+  import type { BookItem } from '../repositories/book';
   import RepositoryFactory, { BOOK } from '../repositories/RepositoryFactory'
   const BookRepository = RepositoryFactory[BOOK]
 
@@ -10,6 +14,10 @@
   /** 取得した配列 */
   let books: BookItem[] = []
   let promise: Promise<void>
+  /** 取得開始ページ */
+  let startIndex = 0;
+  /** データの件数 */
+  let totalItems = 0
 
   /** 検索ボタン押下時の処理 */
   const handleSubmit = () => {
@@ -19,10 +27,36 @@
 
   /** 本一覧取得 */
   const getBooks = async () => {
+    // 配列をリセット
     books = []
+    // apiをコールし、データを取得
     const result = await BookRepository.get({ q })
+    // レスポンスデータをローカル配列に格納
     books = result.items
+    // レスポンスデータの件数を格納
+    totalItems = result.totalItems
   }
+
+  /** ページ数のカウント(watch) */
+  $: hasMore = totalItems > books.length
+
+  /** もっと見るの挙動 */
+  const handleLoadMore = () => {
+    startIndex += 10;
+    promise = getNextPage();
+  }
+
+  /** 次のページへ進む */
+  const getNextPage = async () => {
+    const result = await BookRepository.get({ q, startIndex });
+  
+    // 取得データが既に存在するものを含む可能性があるので、idでフィルタリングしてます。
+    const bookIds = books.map((book) => book.id);
+    const filteredItems = result.items.filter((item) => {
+      return !bookIds.includes(item.id);
+    });
+    books = [...books, ...filteredItems];
+  };
 </script>
 
 <div>
@@ -31,15 +65,17 @@
   </form>
  
   <div class="text-center mt-4">
-
-    {#each books as book (book.id)}
-      <div>{book.volumeInfo.title}</div>
-    {:else}
-      <div>検索結果が見つかりませんでした。</div>
-    {/each}
+    <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
+      {#each books as book (book.id)}
+        <BookCard {book} />
+      {:else}
+        <div>検索結果が見つかりませんでした。</div>
+      {/each}
+    </div>
+    <InfiniteScroll window threshold={100} on:loadMore={handleLoadMore} {hasMore} />
 
     {#await promise}
-      <div>loading...</div>
+      <div class="flex justify-center"><Spinner /></div>
       {:catch e}
         <span class="text-red-600 text-sm">
           {e.message}
